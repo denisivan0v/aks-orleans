c466c1fa8a3ae32712169f5816a973c1eb309816
Denis Ivanov <deivanov@microsoft.com>
2018-12-28 15:30:48 +0100

    init

---
+++ WebApiHost/Startup.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Newtonsoft.Json.Linq;
+
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace WebApiHost
+{
+    public class Startup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Info { Title = "Web API", Version = "v1" }); });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseExceptionHandler(
+                new ExceptionHandlerOptions
+                    {
+                        ExceptionHandler =
+                            async context =>
+                                {
+                                    var feature = context.Features.Get<IExceptionHandlerFeature>();
+                                    var error = new JObject
+                                        {
+                                            { "requestId", context.TraceIdentifier },
+                                            { "code", "unhandledException" },
+                                            { "message", feature.Error.Message }
+                                        };
+
+                                    if (env.IsDevelopment())
+                                    {
+                                        error.Add("details", feature.Error.ToString());
+                                    }
+
+                                    context.Response.ContentType = "application/json";
+                                    await context.Response.WriteAsync(new JObject(new JProperty("error", error)).ToString());
+                                }
+                    });
+
+            app.UseMvc();
+            app.UseSwagger()
+               .UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Web API V1"); });
+        }
+    }
+}
